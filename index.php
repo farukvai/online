90f69033b9171106d8aa19fc100a6d57b1f6fa37
farukvai <farukvai850@gmail.com>
2022-02-16 15:00:07 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=PHP, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Kawser Alom</h2>
+    <?php echo "Hello World"; 
+    
+    if (condition) {
+        # code...
+    }
+    
+    
+    ?>
+</body>
+</html>
